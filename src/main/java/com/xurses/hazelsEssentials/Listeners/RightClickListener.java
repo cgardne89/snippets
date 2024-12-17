0ad4ea203995b25f15855926eb8c8fbfd62342a8
cgard <cgardne89@gmail.com>
2024-12-17 00:50:03 -0500

    testing

--- src/main/java/com/xurses/hazelsEssentials/Listeners/RightClickListener.java
+++ src/main/java/com/xurses/hazelsEssentials/Listeners/RightClickListener.java
@@ -21,7 +21,7 @@ public class RightClickListener implements Listener {
 
         @EventHandler
         public void onPlayerRightClick(PlayerInteractEvent event) {
-
+// chicken butt
 
                 if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
 
